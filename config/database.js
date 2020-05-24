e544e10598b31193ed34efbf963b0357cbc8bb45
marcosreis1976 <marcosaurelio@eris.net.br>
2020-05-24 17:09:57 -0300

    Versão Inicial Marcos

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
+//Trazendo as informaçõe do .env
+require("dotenv").config();
+
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: 'erisintel',
-    database: 'cash',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     define: {
         timestamps: true,
         underscored: true
